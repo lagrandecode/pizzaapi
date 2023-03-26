130042dcc173415f2d7957d423fd1be79c6a7f89
lagrandecode <seunogunmolufirst1@gmail.com>
2023-03-26 22:01:58 +0100

    "commit"

--- pizzaapi/settings.py
+++ pizzaapi/settings.py
@@ -1,17 +1,19 @@
 
 
 from pathlib import Path
-from decouple import config
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+from decouple import config
+SECRET_KEY = config('SECRET_key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG',cast=bool)
