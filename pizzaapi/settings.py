a8db8bc6de25fc5f2d76fc48d706fd1841acddbb
lagrandecode <seunogunmolufirst1@gmail.com>
2023-03-26 21:51:22 +0100

    "commit"

--- pizzaapi/settings.py
+++ pizzaapi/settings.py
@@ -2,6 +2,7 @@
 
 from pathlib import Path
 from decouple import config
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -13,7 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG',cast=bool)
 
 ALLOWED_HOSTS = []
 
@@ -27,6 +28,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'order.apps.OrderConfig',
+    'authentication.apps.AuthenticationConfig',
+    'rest_framework'
+
+
 ]
 
 MIDDLEWARE = [
