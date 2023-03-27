38b5ae27bff2043a652dfed2d2298f1149769613
lagrandecode <seunogunmolufirst1@gmail.com>
2023-03-27 07:25:41 +0100

    "commit"

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.response import Response
 
 # Create your views here.
+
+
+class HelloAuthView(generics.GenericAPIView):
+    def get(self,request):
+        return Response(data={'message':'hello auth'},status=status.HTTP_200_OK)
