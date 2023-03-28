11e86f97d0f611d9c928908e9d9f8c1b1e4aa1d6
lagrandecode <seunogunmolufirst1@gmail.com>
2023-03-28 21:32:56 +0100

    "commit"

--- order/urls.py
+++ order/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.HelloAuthView.as_view()),
+    path('',views.OrderView.as_view()),
 ]
--- order/views.py
+++ order/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import generics,status
+from rest_framework.response import Response
 
 # Create your views here.
+
+class OrderView(generics.GenericAPIView):
+    def get(self,request):
+        return Response(data={'message':'order'},status=status.HTTP_200_OK)
