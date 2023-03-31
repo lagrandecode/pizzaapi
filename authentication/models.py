4acb3aa94627d211d6cc6fcd18c156ac08e68d3f
lagrandecode <seunogunmolufirst1@gmail.com>
2023-03-31 20:03:38 +0100

    "commit"

--- authentication/models.py
+++ authentication/models.py
@@ -12,7 +12,7 @@ class MyUserManager(BaseUserManager):
         if not email:
             raise ValueError('User must have an email address')
         email = self.normalize_email(email)
-        new_user = self.model(email,**extra_fields)
+        new_user = self.model(email=email,**extra_fields)
         new_user.set_password(password)
         new_user.save()
         return new_user
