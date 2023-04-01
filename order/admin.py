dc60db0cc7647851a65bffe9c58aa3315183734f
lagrandecode <seunogunmolufirst1@gmail.com>
2023-04-01 08:47:47 +0100

    "commit"

--- order/admin.py
+++ order/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Order
 
 # Register your models here.
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['size','status','quantity','created_at']
+    list_filter = ['size','status','quantity','created_at']
--- order/models.py
+++ order/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+
+User = get_user_model()
+
+class Order(models.Model):
+    customer = models.ForeignKey(User, on_delete=models.CASCADE)
+    SIZE_CHOICE = (
+        ('SMALL','small'),
+        ('LARGE','Large'),
+        ('EXTRA_LARGE','Extra Large'),
+    )
+    size = models.CharField(max_length=20,choices=SIZE_CHOICE,default=[0][0])
+    ORDER_STATUS = (
+        ('PENDING','Pending'),
+        ('IN_TRANSIT','Transit'),
+        ('DELIVERED','Delivered'),
+    )
+    status = models.CharField(max_length=20,choices=ORDER_STATUS,default=[0][0])
+    quantity = models.PositiveIntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"<Order {self.size} by {self.customer.id}>"
