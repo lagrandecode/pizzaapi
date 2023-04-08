e8731735b0cbdc213aa8e2a8c604234a72f27143
lagrandecode <seunogunmolufirst1@gmail.com>
2023-04-08 12:17:21 +0100

    "commit"

--- order/views.py
+++ order/views.py
@@ -7,3 +7,23 @@ from rest_framework.response import Response
 class OrderView(generics.GenericAPIView):
     def get(self,request):
         return Response(data={'message':'order'},status=status.HTTP_200_OK)
+
+
+
+class OrderCreateListView(generics.GenericAPIView):
+    def get(self,request):
+        pass
+
+    def post(self,request):
+        pass
+
+
+class OrderDetailView(generics.GenericAPIView):
+    def get(self,request,pk):
+        pass 
+
+    def put(self,request,pk):
+        pass
+
+    def delete(self,request,pk):
+        pass
