c894e3881a9adb065cc8fa598c0f72216529a929
lagrandecode <seunogunmolufirst1@gmail.com>
2023-04-10 06:17:24 +0100

    "commit"

--- order/views.py
+++ order/views.py
@@ -72,6 +72,6 @@ class OrderDetailSpecificView(generics.GenericAPIView):
     serializer_class = serializers.OrderDetailSerializer
     def get(self,request,user_id,order_id):
         user = User.objects.get(pk=user_id)
-        order = Order.objects.all().filter(customer=user).filter(pk=order_id)
+        order = Order.objects.all().filter(customer=user).get(pk=order_id)
         serializer = self.serializer_class(instance=order)
         return Response(serializer.data,status=status.HTTP_200_OK)
